f3bf993bfe2656dbab2d5e5cb42f4bf5b51666aa
Daniel Sikar <dsikar@gmail.com>
2020-10-12 15:50:14 +0100

    Added notes on monitoring job.

---
+++ src/traincnn.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Bash script to run batch job on Camber server (@city)
+# To run on camber
+# $ sbatch traincnn.sh
+# To check
+# $ watch -n 1 squeue
+# $ sinfo
+# $ tail -f job<jobid>.out
+# To log into node:
+# ssh <node> # e.g. ssh africa, etc
+#SBATCH --job-name="NVIDIA x Udacity data model"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=daniel.sikar@city.ac.uk
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --output job%J.out
+#SBATCH --error job%J.err
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:1
+
+module load cuda/10.0
+
+python3 train.py --model=../outputs/unity1_nvidia_camber.h5

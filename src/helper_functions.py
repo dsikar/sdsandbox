d015e60152bfe8ec1ef8887918740da25d827d70
Daniel Sikar <dsikar@gmail.com>
2020-10-12 12:34:37 +0100

    Added function for single image prediction

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -73,8 +73,8 @@ def getdict(filepath):
     return mydict
 
 # make dict available
-#filepath = '../dataset/udacity/Ch2_001/final_example.csv'
-#mydict = getdict(filepath)
+# filepath = '../dataset/udacity/Ch2_001/final_example.csv'
+# mydict = getdict(filepath)
 
 if __name__ == "__main__":
     mypred = predict_sa('../outputs/intel_model_qsub_v2.h5',

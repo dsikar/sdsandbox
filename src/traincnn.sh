d808127aa60ed14c11dff988200af06fa59da929
Daniel Sikar <dsikar@gmail.com>
2020-10-18 12:17:25 +0100

    Added slurm script.

--- src/traincnn.sh
+++ src/traincnn.sh
@@ -8,7 +8,11 @@
 # $ sinfo
 # $ tail -f job<jobid>.out
 # To log into node:
-# ssh <node> # e.g. ssh africa, etc
+# $ ssh <node> # e.g. ssh africa, etc
+# To check memory usage on node:
+# $ ps -o pid,user,%mem,command ax | sort -b -k3 -r > procs.txt
+# $ head procs.txt
+
 #SBATCH --job-name="NVIDIA x Udacity data model"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=daniel.sikar@city.ac.uk
@@ -21,4 +25,12 @@
 
 module load cuda/10.0
 
-python3 train.py --model=../outputs/unity1_nvidia_camber.h5
+echo
+echo started job: $(date "+%y%m%d.%H%M%S.%3N")
+echo
+
+python3 train.py --model=../outputs/unity5_nvidia_camber.h5
+
+echo
+echo finished job: $(date "+%y%m%d.%H%M%S.%3N")
+echo
